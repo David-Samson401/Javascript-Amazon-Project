26d1f7c29010c100ad6f3587e4fa26e86c7c4f89
DavidSam <divokorino284@gmail.com>
2024-09-30 13:36:10 +0300

    Save changes

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,20 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+if (!cart) {
+  cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
-}, {
+  }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
+  }];
 }
 
-];
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 
  export function addToCart(productId) {
     let matchingItem;
@@ -25,6 +33,7 @@ export let cart = [{
         quantity: 1
         });
      }
+     saveToStorage();
     }
     
     export function removeFromCart(productId) {
@@ -34,5 +43,21 @@ export let cart = [{
         if (cartItem.productId !== productId) {
           newCart.push(cartItem);
         }
-      })
+      });
+      cart = newCart;
+      saveToStorage();
+    }
+
+    export function updateDeliveryOption(productId, deliveryOptionId) {
+      let matchingItem;
+    
+      cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+    
+      matchingItem.deliveryOptionId = deliveryOptionId;
+    
+      saveToStorage();
     }
